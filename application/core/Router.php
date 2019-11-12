b474099f748f3b86d06763669c4aa4f97a354e6c
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-12 22:05:40 +0900

    add Router.php file and implement Route compiler

---
+++ application/core/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+class Router{
+
+
+    protected $routes;
+
+    public function __construct($definitions){
+
+        $this->routes = $this->complieRoutes($definitions);
+
+    }
+
+    public function compileRoutes($definitions){
+
+        $routes = array();
+
+        foreach($definitions as $url => $params){
+
+            $tokens = explode('/', ltrim($url, '/'));
+
+            foreach($tokens as $i => $token){
+
+                if(0 === strpos($token, ':')){
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name . '>[^/]+)';
+                }
+
+                $tokens[$i] = $token;
+
+            }
+
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
+
+        }
+
+        return $routes;
+
+    }
+}
