2d1a544d532b6aabe70b0c1a59baf87d4c73f7c9
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-13 21:42:41 +0900

    add array_merge function so to say, PDO class

--- application/core/DbManager.php
+++ application/core/DbManager.php
@@ -1,4 +1,32 @@
 <?php
 
 class DbManager{
+// this file try connecting the database with PDO system
+
+    protected $connections = array();
+
+    public function connect($name, $params){
+
+        // functionの引数で与えたparams(array)を下のparams配列にmergeする
+
+        $params = array_merge(array(
+            'dsn' => null,
+            'user' => '',
+            'password' => '',
+            'options' => array()
+        ), $params);
+
+        $con = new PDO(
+            $params['dsn'],
+            $params['user'],
+            $params['password'],
+            $params['options']
+        );
+
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->connections[$name] = $con;
+
+    }
+
 }
