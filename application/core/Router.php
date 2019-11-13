de16be7c2ca1f0d9870d31c55ba3b3734af5f2eb
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-13 15:20:55 +0900

    add resolve function on Router.php file

--- application/core/Router.php
+++ application/core/Router.php
@@ -7,7 +7,9 @@ class Router{
 
     public function __construct($definitions){
 
-        $this->routes = $this->complieRoutes($definitions);
+        // 要は、これによってクラスを作成した時に一番初めに行われる、コンストラクトメソッド
+
+        $this->routes = $this->compileRoutes($definitions);
 
     }
 
@@ -38,4 +40,26 @@ class Router{
         return $routes;
 
     }
+
+    public function resolve($path_info){
+
+        if('/' !== substr($path_info, 0, 1)){
+            $path_info = '/' . $path_info;
+        }
+
+        foreach($this->routes as $pattern => $params){
+
+            if(preg_match('#^' . $pattern . '$#', $path_info, $matches)){
+                $params = array_merge($params, $matches);
+            }
+
+            return $params;
+
+        }
+
+        return false;
+        
+    }
+
+
 }
