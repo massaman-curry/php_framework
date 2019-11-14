054c9adcacb71cbf80e8ada5d5ae7690db471416
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-14 22:19:15 +0900

    add some function to return PDOStatement from given PDO class

--- application/core/DbRepository.php
+++ application/core/DbRepository.php
@@ -3,6 +3,7 @@
 abstract class DbRepository{
     
     protected $con;
+    // $conには、PDOクラスの変数が格納される。
 
     public function __construct($con){
 
@@ -16,8 +17,29 @@ abstract class DbRepository{
 
     }
 
+    public function execute($sql, $params = array()){
 
-    
+        $stmt = $this->con->prepare($sql);
+        // $con、つまり、PDO::prepare($sql)する、ということであり、PDOStatementを返す。
+        $stmt->execute($params);
+        // そして、上のPDOStatementをPDOStatement::executeし、次の行でreturnする。
+        
+        return $stmt;
+
+    }
+
+    public function fetch($sql, $params = array()){
+        // fetch_assocによって、データベースから連想配列で取り出し。fetchとは一行ごとに取り出すこと。
+        return $this->execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
+
+    }
+
+    public function fetchAll($sql, $params = array()){
+
+        return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
 
 }
 
