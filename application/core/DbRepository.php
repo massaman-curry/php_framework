3e879b944149e86476753dddd371866afa950aa3
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-14 21:45:45 +0900

    DbRepository's constructor method.
      add function to help constructor.

--- application/core/DbRepository.php
+++ application/core/DbRepository.php
@@ -1,5 +1,23 @@
 <?php
 
 abstract class DbRepository{
+    
+    protected $con;
+
+    public function __construct($con){
+
+        $this->setConnection($con);
+
+    }
+
+    public function setConnection($con){
+
+        $this->con = $con;
+
+    }
+
+
+    
+
 }
 
