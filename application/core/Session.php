c9ad86ec2a391cd2e209b197eb2c9079aeaa37c2
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-15 21:34:50 +0900

    add constructor method on session.php

--- application/core/Session.php
+++ application/core/Session.php
@@ -2,4 +2,20 @@
 
 class Session{
 
+    protected static $sessionStarted = false;
+    protected static $sessionIdRegenerated = false;
+
+    public function __construct(){
+
+        if(!self::$sessionStarted){
+            // $sessionStartedがfalseの時に、session_start()を実行
+            session_start();
+
+            self::$sessionStarted = true;
+
+        }
+
+    }
+
+
 }
