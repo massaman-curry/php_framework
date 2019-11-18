25dbe86e72c70840f64a2be58bd1f57d2dacb2d3
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-18 17:41:24 +0900

    implement initialize method on Application class

--- application/core/Application.php
+++ application/core/Application.php
@@ -1,4 +1,46 @@
 <?php
 
 abstract class Application{
+
+    protected $debug = false;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($debug = false){
+
+        $this->setDebugMode($debug);
+        $this->initialize();
+        $this->configure();
+
+    }
+
+    protected function setDebugMode($debug){
+
+        if($debug){
+
+            $this->debug = true;
+            ini_set('display_errors', 1);
+            error_reporting(-1);
+
+        }else{
+
+            $this->debug = false;
+            ini_set('display_errors', 0);
+
+        }
+
+    }
+
+    protected function initialize(){
+
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->session = new Session();
+        $this->db_manager = new DbManager();
+        $this->router = new Router($this->registerRoutes());
+
+    }
+
 }
