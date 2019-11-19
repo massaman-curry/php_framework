32a299466c03f960116c0fa8e02be1ef8d7aad86
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-19 22:36:30 +0900

    add function to render 404 not found

--- application/core/Application.php
+++ application/core/Application.php
@@ -177,5 +177,16 @@ abstract class Application{
 
     }
 
+    protected function render404Page($e){
+
+        $this->response->setStatusCode(404, 'Not Found');
+
+        $message = $this->isDebugMode() ? $e->getMessage() : 'Page not found.';
+        $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+
+        $this->response->setContent()
+
+    }
+
 
 }
