b001aec02c7781aad62d1de39aafbd6e6d856204
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-20 21:57:50 +0900

    add method to execute given object

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -11,6 +11,8 @@ abstract class Controller{
     protected $db_manager;
 
     public function __construct($application){
+    // $applicationはクラスを定義した時の引数。クラスからインスタンスを生成するときに、
+    // 引数の中にオブジェクトを入れることになるはず
 
         $this->controller_name = strtolower(substr(get_class($this), 0, -10));
         // おそらく、クラス名をExampleControllerという形で命名し、その'Controller'というところを省いたものを
@@ -20,6 +22,21 @@ abstract class Controller{
         $this->response = $application->getResponse();
         $this->session = $application->getSession();
         $this->db_manager = $application->getDbManager();
+
+    }
+
+    public function run($action, $params = array()){
+
+        $this->action_name = $action;
+        $action_method = $action . 'Action';
+
+        if (!method_exists($this, $action_method)){
+            $this->forward404();
+        }
+
+        $content = $this->$action_method($params);
+
+        return $content;
 
     }
 
