3c17a57ba67ce93df5f4849389bf25c1fdd3f9f4
massaman_curry <m.m.elcapitan@gmail.com>
2019-11-26 15:41:06 +0900

    implement constractor on view file

--- application/core/View.php
+++ application/core/View.php
@@ -1,5 +1,18 @@
 <?php
 
 class View{
+
+    protected $base_dir;
+    protected $defaults;
+    protected $layout_variables = array();
+
+    public function __construct($base_dir, $defaults = array()){
+
+        $this->base_dir = $base_dir;
+        $this->defaults = $defaults;
+
+    }
+
+    
 }
 
